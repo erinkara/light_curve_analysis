2956e8af46b6b133f5435efefbd8a9504f467cd4
Erin Kara <ekara@ast.cam.ac.uk>
2015-03-02 16:28:29 +0000

    Now cross.py can average over many observations
    Can now specify  arbitrary number of observation in input string,
    e.g. --lcfiles "obs1_soft obs1_hard obs2_soft obs2_hard...""

--- cross.py
+++ cross.py
@@ -5,21 +5,22 @@ from scipy.stats import binned_statistic
 import matplotlib.pyplot as plt
 
 
-def take_fourier_transform(lc1, lc2, dt, flo, fhi, nbins):
+def calculate_lag(lc1, lc2, dt, flo, fhi, nbins):
     
-    n = len(lc1)
-    
+
+    n = lc1.shape[1]
+ 
     # take fourier transform
     fs = np.fft.fft(lc1)
     fh = np.fft.fft(lc2)
-    print fs
     freq = np.fft.fftfreq(n,d=dt)
+    freq = np.array(fs.shape[0]*[freq])
 
     # only use positive frequencies. fs[0]=0
-    fs = fs[1:n/2]
-    fh = fh[1:n/2]
-    freq = freq[1:n/2]
-    m = len(freq)
+    fs = fs[:,1:n/2]
+    fh = fh[:,1:n/2]
+    freq = freq[:,1:n/2]
+    m = freq.shape[1]
 
     # calculate cross spectrum and power spectra
     c = (2.*dt/m) * fs * fh.conjugate()
@@ -34,16 +35,40 @@ def take_fourier_transform(lc1, lc2, dt, flo, fhi, nbins):
     fbounds = np.logspace(flo, fhi, nbins+1)
 
     # bin cross and power spectra
-    avgc_real = binned_statistic(freq, c_real, statistic='mean', bins=fbounds)[0]
-    avgc_imag = binned_statistic(freq, c_imag, statistic='mean', bins=fbounds)[0]
-    avgpows = binned_statistic(freq, pows, statistic='mean', bins=fbounds)[0]
-    avgpowh = binned_statistic(freq, powh, statistic='mean', bins=fbounds)[0]
-    nbinned = binned_statistic(freq, pows, statistic='count', bins=fbounds)[0]
+    # make a for loop to go through each lc
+    avgc_real, avgc_imag, avgpows, avgpowh, nbinned = [], [], [], [], []
+    for i in range(pows.shape[0]):
+        avgc_real.append(binned_statistic(freq[i], c_real[i], statistic='mean', bins=fbounds)[0])
+        avgc_imag.append(binned_statistic(freq[i], c_imag[i], statistic='mean', bins=fbounds)[0])
+        avgpows.append(binned_statistic(freq[i], pows[i], statistic='mean', bins=fbounds)[0])
+        avgpowh.append(binned_statistic(freq[i], powh[i], statistic='mean', bins=fbounds)[0])
+        nbinned.append(binned_statistic(freq[i], pows[i], statistic='count', bins=fbounds)[0])
+    avgc_real = np.array(avgc_real)
+    avgc_imag = np.array(avgc_imag)
+    avgpows = np.array(avgpows)
+    avgpowh = np.array(avgpowh)
+    nbinned = np.array(nbinned)
 
     # convert avgc back into complex array
     avgc = avgc_real + 1j*avgc_imag
 
-    return avgc, avgpows, avgpowh, nbinned, fbounds
+    avgc = np.mean(avgc, axis=0)
+    avgpows = np.mean(avgpows, axis=0)
+    avgpowh = np.mean(avgpowh, axis=0)
+    nbinned = np.sum(nbinned, axis=0)
+
+    # get frequency bins
+    fbin = (fbounds[1:] + fbounds[:-1])/2.
+    dfbin = (fbounds[1:] - fbounds[:-1])/2.
+
+    # calculate lag and lag error
+    phase = np.angle(avgc)
+    lag = phase/(2*np.pi*fbin)
+    g = abs(avgc)**2/(avgpows * avgpowh)
+    dphase = nbinned**(-0.5) * np.sqrt((1.-g)/(2.*g))
+    dlag = dphase/(2*np.pi*fbin)
+
+    return fbin, dfbin, lag, dlag, avgpows, avgpowh, nbinned
 
 def lag_freq():
 
@@ -64,31 +89,23 @@ def lag_freq():
     fnbin = args.fnbin
     ## ------------------------------------
 
+
     nlc = len(lcfiles)/2
     lcs = []
     lch = []  
-    for i in range(nlc):
-    	data_soft = np.loadtxt(lcfiles[i], unpack=True)
-    	data_hard = np.loadtxt(lcfiles[i+1], unpack=True)
-
-	dt = data_soft[0][1] - data_soft[0][0]
-	lcs.append(data_soft[1])
-	lch.append( data_hard[1])
+    c = 0
+    for lc in range(nlc):
+    	data_soft = np.loadtxt(lcfiles[c], unpack=True)
+    	data_hard = np.loadtxt(lcfiles[c+1], unpack=True)
+        dt = data_soft[0][1] - data_soft[0][0]
+        lcs.append(data_soft[1])
+        lch.append( data_hard[1])
+        c+=2
     lcs = np.array(lcs)
     lch = np.array(lch)
 
-    avgc,avgpows,avgpowh, nbinned, fbounds = take_fourier_transform(lcs, lch, dt, np.log10(flo), np.log10(fhi), fnbin)
+    fbin, dfbin, lag, dlag, avgpows, avgpowh, nbinned = calculate_lag(lcs, lch, dt, np.log10(flo), np.log10(fhi), fnbin)
 
-    # get frequency bins
-    fbin = (fbounds[1:] + fbounds[:-1])/2.
-    dfbin = (fbounds[1:] - fbounds[:-1])/2.
-
-    # calculate lag and lag error
-    phase = np.angle(avgc)
-    lag = phase/(2*np.pi*fbin)
-    g = abs(avgc)**2/(avgpows * avgpowh)
-    dphase = nbinned**(-0.5) * np.sqrt((1.-g)/(2.*g))
-    dlag = dphase/(2*np.pi*fbin)
 
     # plot
     fig = plt.figure()
@@ -103,9 +120,10 @@ def lag_freq():
     ax.set_xscale('log')
     ax.set_title("Lag")
     l = plt.axhline(y=1)
-    ax.set_xlim(fbounds[0],fbounds[-1])
+    ax.set_xlim(fbin[0]-dfbin[0],fbin[-1]+dfbin[-1])
     plt.show()
 
 if __name__ == "__main__":
+    import ipdb
     lag_freq()
 
