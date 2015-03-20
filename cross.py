a462332f071758f392d4f0eb1984da3f166edb04
erinkara <kara.erin@gmail.com>
2015-03-20 16:54:05 +0000

    cross.py is better.

--- cross.py
+++ cross.py
@@ -3,17 +3,21 @@ import argparse
 import numpy as np
 from scipy.stats import binned_statistic
 import matplotlib.pyplot as plt
+import pyfftw
 
-
-def calculate_lag(lc1, lc2, dt, flo, fhi, nbins):
-    
+def calculate_crossspec(lc1, lc2, dt, flo, fhi, nbins):
 
     n = lc1.shape[1]
+    nlc = lc1.shape[0]
  
+    # mean of light curves for rms normalization
+    mean_lc1 = np.mean(lc1, axis=1)
+    mean_lc2 = np.mean(lc2, axis=1)
+
     # take fourier transform
-    fs = np.fft.fft(lc1)
-    fh = np.fft.fft(lc2)
-    freq = np.fft.fftfreq(n,d=dt)
+    fs =  pyfftw.interfaces.numpy_fft.fft(lc1)
+    fh = pyfftw.interfaces.numpy_fft.fft(lc2)
+    freq = pyfftw.interfaces.numpy_fft.fftfreq(n,d=dt)
     freq = np.array(fs.shape[0]*[freq])
 
     # only use positive frequencies. fs[0]=0
@@ -23,13 +27,13 @@ def calculate_lag(lc1, lc2, dt, flo, fhi, nbins):
     m = freq.shape[1]
 
     # calculate cross spectrum and power spectra
-    c = (2.*dt/m) * fs * fh.conjugate()
-    pows = (2.*dt/m) * np.abs(fs)**2
-    powh = (2.*dt/m) * np.abs(fh)**2
+    c = (2.*dt/(m*mean_lc1*mean_lc2)) * fs * fh.conjugate()
+    pows = (2.*dt/(m*mean_lc1)) * np.abs(fs)**2
+    powh = (2.*dt/(m*mean_lc2)) * np.abs(fh)**2
 
     # take real and imaginary part of cross (for binning purposes)
     c_real = c.real
-    c_imag = c.imag  
+    c_imag = c.imag 
 
     # calculate the bounds of the frequency bins
     fbounds = np.logspace(flo, fhi, nbins+1)
@@ -40,8 +44,8 @@ def calculate_lag(lc1, lc2, dt, flo, fhi, nbins):
     for i in range(pows.shape[0]):
         avgc_real.append(binned_statistic(freq[i], c_real[i], statistic='mean', bins=fbounds)[0])
         avgc_imag.append(binned_statistic(freq[i], c_imag[i], statistic='mean', bins=fbounds)[0])
-        avgpows.append(binned_statistic(freq[i], pows[i], statistic='mean', bins=fbounds)[0])
-        avgpowh.append(binned_statistic(freq[i], powh[i], statistic='mean', bins=fbounds)[0])
+        avgpows.append(binned_statistic(freq[i], fs.imag[i], statistic='mean', bins=fbounds)[0])
+        avgpowh.append(binned_statistic(freq[i], fh.imag[i], statistic='mean', bins=fbounds)[0])
         nbinned.append(binned_statistic(freq[i], pows[i], statistic='count', bins=fbounds)[0])
     avgc_real = np.array(avgc_real)
     avgc_imag = np.array(avgc_imag)
@@ -55,11 +59,17 @@ def calculate_lag(lc1, lc2, dt, flo, fhi, nbins):
     avgc = np.mean(avgc, axis=0)
     avgpows = np.mean(avgpows, axis=0)
     avgpowh = np.mean(avgpowh, axis=0)
+    davgpows = np.mean(avgpows, axis=0)/np.sqrt(nlc)
+    davgpowh = np.mean(avgpowh, axis=0)/np.sqrt(nlc)
     nbinned = np.sum(nbinned, axis=0)
 
     # get frequency bins
     fbin = (fbounds[1:] + fbounds[:-1])/2.
     dfbin = (fbounds[1:] - fbounds[:-1])/2.
+
+    return fbin, dfbin, avgc, avgpows, davgpows, avgpowh, davgpowh, nbinned
+
+def calculate_lag(avgc, avgpows, avgpowh, nbinned, fbin):
 
     # calculate lag and lag error
     phase = np.angle(avgc)
@@ -68,54 +78,45 @@ def calculate_lag(lc1, lc2, dt, flo, fhi, nbins):
     dphase = nbinned**(-0.5) * np.sqrt((1.-g)/(2.*g))
     dlag = dphase/(2*np.pi*fbin)
 
-    return fbin, dfbin, lag, dlag, avgpows, avgpowh, nbinned
+    return lag, dlag
 
-def xmm_lag_freq():
+def calculate_coherence(avgc, avgpows, avgpowh, nbinned, fbin):
 
-    nlc = len(lcfiles)/2
-    lcs = []
-    lch = []  
-    c = 0
-    for lc in range(nlc):
-    	data_soft = np.loadtxt(lcfiles[c], unpack=True)
-    	data_hard = np.loadtxt(lcfiles[c+1], unpack=True)
-        dt = data_soft[0][1] - data_soft[0][0]
-        lcs.append(data_soft[1])
-        lch.append( data_hard[1])
-        c+=2
-    lcs = np.array(lcs)
-    lch = np.array(lch)
+    # to do
 
-    fbin, dfbin, lag, dlag, avgpows, avgpowh, nbinned = calculate_lag(lcs, lch, dt, np.log10(flo), np.log10(fhi), fnbin)
+def plot_powspec(fbin, dfbin, avgpows, davgpows, avgpowh, davgpowh):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.errorbar(fbin, avgpows,
+        xerr=dfbin, 
+	yerr=davgpows,
+	capsize=0,
+	linestyle='None',
+	marker='o',
+	color='black')
+    ax.errorbar(fbin, avgpowh,
+        xerr=dfbin, 
+	yerr=davgpowh,
+	capsize=0,
+	linestyle='None',
+	marker='s',
+	color='red')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_title("Power Spectra")
+    ax.set_xlim(fbin[0]-dfbin[0],fbin[-1]+dfbin[-1])
+    plt.show()
+    plt.savefig('%s_powspec.png')%(name)
 
-def nustar_lag_freq():
+    # print outfile
+    fp = open( '%s_powspec_4vsz.dat'%(name), 'w' )
+    out = 'descriptor freq,+- pows,+- powh,+-\n'
+    out += ''.join('%3.3g %3.3g %3.3g %3.3g\n'%(fbin[i],dfbin[i],\
+       avgpows[i],davgpows[i],avgpowh[i],davgpowh[i]) for i in range(len(fbin)))
+    fp.write(out)
+    fp.close()
 
-    data_lcA = np.loadtxt(lcfiles[0], unpack=True)
-    data_lcB = np.loadtxt(lcfiles[1], unpack=True)
-
-    t = data_lcA[0]
-    dt = t[1] - t[0]
-
-    t_edge = []
-    for i in range(len(t)-1):
-        if t[i+1] - t[i] > dt*10:
-            continue
-        else:
-            t_edge.append(i)
-    t_edge = np.array(t_edge)
-
-    data_lcA = np.array_split(data_lcA, t_edge, axis=1)
-    data_lcB = np.array_split(data_lcB, t_edge, axis=1)
-
-    lcA_soft = data_lcA[1]
-    lcB_soft = data_lcB[1]
-    lcA_hard = data_lcA[3]
-    lcB_hard = data_lcB[3]
-    
-    fbin, dfbin, lag, dlag, avgpows, avgpowh, nbinned = calculate_lag(lcs, lch, dt, np.log10(flo), np.log10(fhi), fnbin)
-
-def plot_lag()
-    # plot
+def plot_lag(fbin, dfbin, lag, dlag):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.errorbar(fbin, lag,
@@ -127,21 +128,62 @@ def plot_lag()
 	color='black')
     ax.set_xscale('log')
     ax.set_title("Lag")
-    l = plt.axhline(y=1)
+    l = plt.axhline(y=0)
     ax.set_xlim(fbin[0]-dfbin[0],fbin[-1]+dfbin[-1])
     plt.show()
+    plt.savefig('%s_lag.png')%(name)
 
-if __name__ == "__main__":
-    import ipdb
+    # print outfile
+    fp = open( '%s_lag_4vsz.dat'%(name), 'w' )
+    out = 'descriptor freq,+- lag,+-\n'
+    out += ''.join('%3.3g %3.3g %3.3g %3.3g\n'%(fbin[i],dfbin[i],\
+       lag[i],dlag[i]) for i in range(len(fbin)))
+    fp.write(out)
+    fp.close()
+
+def plot_coher(fbin, dfbin, coher, dcoher):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.errorbar(fbin, coher,
+        xerr=dfbin, 
+	yerr=dcoher,
+	capsize=0,
+	linestyle='None',
+	marker='o',
+	color='black')
+    ax.set_xscale('log')
+    ax.set_title("Coherence")
+    l = plt.axhline(y=0)
+    l = plt.axhline(y=1)
+    ax.set_xlim(fbin[0]-dfbin[0],fbin[-1]+dfbin[-1])
+    ax.set_ylim(-0.2,1.2)
+    plt.show()
+    plt.savefig('%s_coher.png')%(name)
+
+    # print outfile
+    fp = open( '%s_coher_4vsz.dat'%(name), 'w' )
+    out = 'descriptor freq,+- coher,+-\n'
+    out += ''.join('%3.3g %3.3g %3.3g %3.3g\n'%(fbin[i],dfbin[i],\
+       coher[i],dcoher[i]) for i in range(len(fbin)))
+    fp.write(out)
+    fp.close()
+
+def main():
     p = argparse.ArgumentParser(
-        description="Calculate lags",
+        description="Calculate lag-frequency spectra",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     p.add_argument("--lcfiles"    , metavar="lcfiles", type=str, default='',help="String with names of input files")
     p.add_argument("--flo"  , metavar="flo", type=float, default=1e-4, help="Lower Bound Frequency")
     p.add_argument("--fhi"  , metavar="fhi", type=float, default=1e-2, help="Upper Bound Frequency")
+    p.add_argument("--soft"  , metavar="flo", type=str, default="0.3,1", help="Soft band energies, e.g. "0.3,1"")
+    p.add_argument("--hard"  , metavar="fhi", type=str, default="1,4", help="Hard band energies, e.g. "1,4"")
     p.add_argument("--fnbin"  , metavar="fnbin", type=float, default=10, help="Number of Frequency Bins")
-    p.add_argument("--instr"  , metavar="instr", type=str, default='xmm', help="xmm or nustar?")
+    p.add_argument('--lag', action='store_true',default=True,help='Set to make lag spectrum')
+    p.add_argument('--coher', action='store_true',default=False,help='Set to make coher spectrum')
+    p.add_argument("--name"    , metavar="name", type=str, default='',help="The root name of the output files.")
+    p.add_argument('--psd', action='store_true',default=False,help='Set to make hard and soft band PSDs')
+    p.add_argument('--npz', action='store_true',default=False,help='Set use .npz files instead of ascii. Default=False')
     args = p.parse_args()
 
     ## ------------------------------------
@@ -149,13 +191,49 @@ if __name__ == "__main__":
     flo    = args.flo
     fhi = args.fhi
     fnbin = args.fnbin
-    instr = args.instr
+    soft = np.array(args.soft.split())
+    hard = np.array(args.hard.split())
     ## ------------------------------------
 
-    if instr == 'xmm':
-        xmm_lag_freq()
-    else:
-        nustar_lag_freq()
+    # calculate energy bins 
+    f = open(lcfiles[0], 'r')
+    enbins = np.array(f.readline().split()[3:-1],float)
 
-    plot_lag()
+    lcs = lcs[(np.where((enbins>soft[0]) & (enbins<=soft[1]))),:]
+    lch = lch[(np.where((enbins>hard[0]) & (enbins<=hard[1]))),:]
+
+
+    lc = []  
+    for l in lcfiles:
+        if args.npz:
+	    data = np.load(l)
+	    data = data['arr_0'].T
+        else:
+	    data = np.loadtxt(l, unpack=True)
+        dt = data[0][1] - data[0][0]
+        lc.append(data[1])
+    lc = np.array(lc)
+    print lcs.shape
+
+    fbin, dfbin, avgc, avgpows, davgpows, avgpowh, davgpowh, nbinned = \
+        calculate_crossspec(lcs, lch, dt, np.log10(flo), np.log10(fhi), fnbin)
+
+    if args.powspec:
+        print_powspec(fbin, dfbin, avgpows, davgpows, avgpowh, davgpowh)
+
+    if args.lag:
+        lag, dlag = calculate_lag(avgc, avgpows, avgpowh, nbinned, fbin)
+        print_lag(fbin, dfbin, lag, dlag)
+
+    if args.coher:
+        coher, dcoher = calculate_coher(avgc, avgpows, avgpowh, nbinned, fbin)
+        print_coher(fbin, dfbin, coher, dcoher)
+
+    #### how to deal with adding small energy bins together
+    #### coherence
+    #### dealing with npz files (argument)
+
+
+if __name__ == "__main__":
+    import ipdb
 
